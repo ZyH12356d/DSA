181c9f05a0fb54d56f43002b0de93c208655fe2e
ZyH12356d <107683128+ZyH12356d@users.noreply.github.com>
2025-10-23 04:47:49 +0630

    Add project files.

---
+++ DSA.Core/SortingAlgo/LinearSearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Core.SortingAlgo
+{
+    public class LinearSearch
+    {
+        public static int Search(int[]? arr , int num)
+        {
+            if (arr == null || arr.Length == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] == num)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+        }
+    }
+}
---
+++ DSA.Tests/SortingTests/LinearSearchTests.cs
@@ -0,0 +1,61 @@
+﻿using DSA.Core.SortingAlgo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Tests.SortingTests
+{
+    public class LinearSearchTests
+    {
+        [Fact]
+        public void testEmpty()
+        {
+            int[] array = { };
+            int index = LinearSearch.Search(array, 10);
+            Assert.Equal(-1, index);
+        }
+        [Fact]
+        public void testNull()
+        {
+            int[]? array = null;
+            int index = LinearSearch.Search(array, 10);
+            Assert.Equal(-1, index);
+        }
+        [Fact]
+        public void testSingleElement()
+        {
+            int[] array = {10};
+            int index = LinearSearch.Search(array, 10);
+            Assert.Equal(0, index);
+        }
+        [Fact]
+        public void testBoundaryCase()
+        {
+            int[] array = {10 , 11};
+            int index = LinearSearch.Search(array, 11);
+            Assert.Equal(1, index);
+            
+            index = LinearSearch.Search(array, 10);
+            Assert.Equal(0, index);
+        }
+        [Fact]
+        public void testUnHappyCase()
+        {
+            int[] array = {10 , 11};
+            int index = LinearSearch.Search(array, 12);
+            Assert.Equal(-1, index);
+            
+        }
+        [Fact]
+        public void testDuplicateCase()
+        {
+            int[] array = {10 , 11 , 19 , 21 , 30 , 11 , 19};
+            int index = LinearSearch.Search(array, 19);
+            Assert.Equal(2, index);
+            
+        }
+
+    }
+}
